f8d59f3b48a26fd7169b2247e6b0ceefe47f4248
amaan19 <amaan.ahmed3@gmail.com>
2018-12-12 17:28:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+class School
+  def initialize (name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+attr_reader :roster
+
+
+ def add_student(name, grade)
+   if self.roster[grade]
+     self.roster[grade] << name
+   else
+   self.roster[grade] = [] << name
+end
+end
+
+def grade (grade)
+  self.roster[grade]
+end
+
+def sort
+  self.roster.each {|grade, names|
+    names.sort!
+    }
+end
+end
+#binding.pry
+'eoi'
